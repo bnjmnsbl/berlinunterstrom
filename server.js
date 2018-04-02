a26bb445c70f30973ddf68433af575ff6747bde3
Benjamin <bs@MacBook-Pro.local>
2018-04-02 13:40:31 +0200

    removed modules

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const axios = require("axios");
 const xml2js = require("xml2js");
 const util = require("util"); //dev only
-const builder = require("./builder.js");
+
 const express = require("express");
 const app = express();
 
 const parser = new xml2js.Parser();
+const builder = require("./builder.js");
 
-//var xmls = "<smeterengine>\n <scale>DAY</scale>\n <city>BERLIN</city>\n <district>\n <time_period begin=\"2013-06-12 15:40:00\" end=\"2013-06-12 17:00:00\" time_zone='CET'/>\n</district>\n</smeterengine> ";
-//var xmls = "<smeterengine> <scale>DAY</scale> <city>BERLIN</city> <district> <time_period begin=\"2013-06-12 15:40:00\" end=\"2013-06-12 17:00:00\" time_zone='CET'/></district></smeterengine> ";
 
 var xmls = builder.prepareOptions("DAY", "BERLIN", "Pankow", "2013-06-12 15:40:00", "2013-06-12 17:00:00");
 
+app.get("/", function(req,res) {
+  res.sendFile("./index.html");
+});
+
+/*
 axios.post('https://www.vattenfall.de/SmeterEngine/networkcontrol',
            xmls,
            {headers:
@@ -23,3 +27,7 @@ axios.post('https://www.vattenfall.de/SmeterEngine/networkcontrol',
              });
            }).catch(err=>{console.log(err)
 });
+
+*/
+
+app.listen(8080, () => {console.log("listening on port 8080")})
